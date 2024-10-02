cc6b3f831b295df738255e6688b2e1b4b6a60e04
gavin <gavinrvsmith@gmail.com>
2024-10-02 10:09:07 -0700

    insert, searches

--- datastructures/avltree.py
+++ datastructures/avltree.py
@@ -1,12 +1,13 @@
-from typing import Callable, Generic, Iterable, List
-from datastructures.iavltree import IAVLTree, K, V
 from __future__ import annotations
+from typing import Callable, Generic, Iterable, List, Optional
+from datastructures.iavltree import IAVLTree, K, V
 
 
 class Node(Generic[K,V]):
     Left: Node[K,V] = None
     Right: Node[K,V] = None
-    Height = 0
+    Height = 1
+    bf = 0
     Key: K
     Value: V 
     def __init__(self, key: K, val: V) -> None:
@@ -14,35 +15,46 @@ class Node(Generic[K,V]):
         self.Value = val
 
     def recalc_height(self):
-        self.Height = 1 + max(self.Left.Height or 0, self.Right.Height or 0)
+        lHeight = 0 if not self.Left else self.Left.Height
+        rHeight = 0 if not self.Right else self.Right.Height
+
+        self.Height = 1 + max(lHeight, rHeight)
+        self.bf = lHeight - rHeight
+
+    def __repr__(self) -> str:
+        return f"Node[{self.Key}]"
 
 
 class AVLTree(IAVLTree[K,V], Generic[K,V]):
     Root: Node[K,V] = None
     Count = 0
     
-    def __init__(self, seq: Iterable[tuple[K,V]]) -> None:
+    def __init__(self, seq: Iterable[tuple[K,V]] = None) -> None:
         for k,v in seq or []: self.insert(k,v)
 
-    def insert(self, key: K, val: V) -> None:
+    def insert(self, key: K, val: V = None) -> None:
+        self.Count += 1
         if self.Root is None:
             self.Root = Node(key, val)
             return
-        self.insert_helper(Node(key,val), self.Root)
-        self.Count += 1
+        self.Root = self.insert_helper(Node(key,val), self.Root)
         
 
     def insert_helper(self, newNode: Node[K,V], node: Node[K,V]) -> Node[K,V]:
-
-        child = node.Left if newNode.Key < node.Key else node.Right
-        if child is None:
-            child = newNode
+        if node is None:
             return newNode
-        else:
-            self.insert_helper(newNode, child)
-            node.Height = 1 + max(node.Left.Height or 0, node.Right.Height or 0)
 
-            #TODO balance factor & rotations
+        if (newNode.Key < node.Key):
+            node.Left = self.insert_helper(newNode, node.Left)
+        else:
+            node.Right = self.insert_helper(newNode, node.Right)
+        
+        node.recalc_height()
+
+        if node.bf > 1 or node.bf < -1:
+            return self.balance_tree(node)
+
+        return node
 
 
     def balance_tree(self, node: Node[K,V]) -> Node[K,V]:
@@ -76,24 +88,101 @@ class AVLTree(IAVLTree[K,V], Generic[K,V]):
         return root
 
     def search(self, key: K) -> V | None:
-        raise NotImplementedError
+        def recurse(node: Node[K,V]) -> V:
+            child = node.Left if key < node.Key else node.Right
+            return recurse(child) if child is not None else None
+
+        return recurse(self.Root)
 
     def delete(self, key: K) -> None:
 
         self.Count -= 1
         raise NotImplementedError
+    
+    #TODO: test delete
+    
+    def delete_helper(self, node: Node[K,V], key: K) -> Node[K,V]:
+        if node is None:
+            return None
+        
+        if (key < node.Key):
+            node.Left = self.insert_helper(node.Left, key)
+        else:
+            node.Right = self.insert_helper(node.Right, key)
+        
+        #find successor
+        successor = self.find_sucessor(node)
+        node.Key = successor.Key
+        node.Value = successor.Value
+        node.Right = self.delete_helper(node.Right, successor.Key)
+
+        if node is not None:
+            node.recalc_height()
+            if node.bf > 1 or node.bf < -1:
+                return self.balance_tree(node)
+
+        return None
+    
+    def find_sucessor(self, node: Node[K,V]) -> Node[K,V]:
+        if node.Left is None or node.Right is None:
+            return node.Left or node.Right
+        current = node.Right
+        while current.Left is not None:
+            current = current.Left
+        return current
 
     def inorder(self, visit: Callable[[V], None] | None = None) -> List[K]:
-        raise NotImplementedError
-
+        def recurse(l: list[K], node: Node[K,V]) -> list[K]:
+            if node.Left: recurse(l, node.Left)
+            if visit: visit(node.Value)
+            l.append(node.Value)
+            if node.Right: recurse(l, node.Right)
+            return l
+        return recurse([], self.Root)
+    
     def preorder(self, visit: Callable[[V], None] | None = None) -> List[K]:
-        raise NotImplementedError
-
+        def recurse(l: list[K], node: Node[K,V]) -> list[K]:
+            if visit: visit(node.Value)
+            l.append(node.Value)
+            if node.Left: recurse(l, node.Left)
+            if node.Right: recurse(l, node.Right)
+            return l
+        return recurse([], self.Root)
+    
     def postorder(self, visit: Callable[[V], None] | None = None) -> List[K]:
-        raise NotImplementedError
+        def recurse(l: list[K], node: Node[K,V]) -> list[K]:
+            if node.Left: recurse(l, node.Left)
+            if node.Right: recurse(l, node.Right)
+            if visit: visit(node.Value)
+            l.append(node.Value)
+            return l
+        return recurse([], self.Root)
 
     def bforder(self, visit: Callable[[V], None] | None = None) -> List[K]:
-        raise NotImplementedError
+        stack = [self.Root]
+        l = [self.Root.Value]
+        while len(stack) != 0:
+            node = stack.pop()
+            if node.Right is not None:
+                stack.append(node.Right)
+            if node.Left is not None:
+                stack.append(node.Left)
+                l.append(node.Left.Value)
+            if node.Right is not None:
+                l.append(node.Right.Value)
+        return l
+    
+    #TODO: fix inorder
 
     def size(self) -> int:
         return self.Count
+
+    def __str__(self) -> str:
+        level_outputs = []
+        def draw_tree(node: Optional[Node], level: int = 0):
+            if not node: return
+            draw_tree(node.Right, level + 1)
+            level_outputs.append(f"{" "*4*level} -> {str(node.Value)}")
+            draw_tree(node.Left, level + 1)
+        draw_tree(self.Root)
+        return '\n'.join(level_outputs)
--- program.py
+++ program.py
@@ -1,3 +1,5 @@
+from datastructures.avltree import AVLTree
+from datastructures.test_avltree_inserts import TestAVLInserts
 from tests.car import Car, Color, Make, Model
 
 def main():
@@ -7,4 +9,21 @@ def main():
     print(car)
 
 if __name__ == '__main__':
-    main()
+    # main()
+    # tree = AVLTree()
+    # tree.insert(6)
+    # tree.insert(4)
+    # tree.insert(2)
+    tree = AVLTree[int, int]()
+
+    for node in [8, 9, 10, 2, 1, 5, 3, 6, 4, 7]:
+        tree.insert(node, node)
+
+    assert tree.inorder() == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+    print(str(tree))
+
+    assert tree.bforder() == [5, 3, 8, 2, 4, 6, 9, 1, 7, 10]
+
+
+    TestAVLInserts()
